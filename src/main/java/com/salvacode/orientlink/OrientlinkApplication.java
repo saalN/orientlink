4631695523138382ed717c0645eca8218db3fbaa
saalN <ususale22@gmail.com>
2025-12-09 18:09:18 +0100

    First commit

---
+++ src/main/java/com/salvacode/orientlink/OrientlinkApplication.java
@@ -0,0 +1,13 @@
+package com.salvacode.orientlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientlinkApplication.class, args);
+	}
+
+}
